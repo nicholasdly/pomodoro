9e5071c5d55aa5f3c0bb406134252ebc064b4a3a
Nicholas Ly <nichdly@gmail.com>
2022-12-05 14:29:05 -0500

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,16 @@ export default function Footer() {
         <div className="bg-neutral-900 text-center w-[100vw] p-4">
 
             <p>
-                Checkout my website at {}
+                Tomatera is a student's individual web development project,
+                <br/>
+                and will stay free to use and free of ads <b>forever</b>.
+                <br/>
+            </p>
+            <br/>
+            <p>
+                If there are any issues, please contact me through my website.
+                <br/>
+                Check it out at {}
                 <a className="underline" href="https://nicholasly.com/" target="_blank" rel="noreferrer noopener">
                     nicholasly.com
                 </a>
